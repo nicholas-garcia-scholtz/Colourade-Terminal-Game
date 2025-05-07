460f3757418ed06b4a0bbcf3bc611f912571dca8
nasuoa <17995588+nasuoa@users.noreply.github.com>
2025-05-07 12:52:22 +1200

    Initial commit

---
+++ src/main/java/nz/ac/auckland/se281/engine/Game.java
@@ -0,0 +1,15 @@
+package nz.ac.auckland.se281.engine;
+
+import nz.ac.auckland.se281.Main.Difficulty;
+
+public class Game {
+  public static String AI_NAME = "HAL-9000";
+
+  public Game() {}
+
+  public void newGame(Difficulty difficulty, int numRounds, String[] options) {}
+
+  public void play() {}
+
+  public void showStats() {}
+}
