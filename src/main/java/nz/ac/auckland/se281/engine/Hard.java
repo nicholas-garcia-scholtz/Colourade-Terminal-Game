81115438416cdd702e256834c185ba91a453ddee
nicholas-garcia-scholtz <ngar756@aucklanduni.ac.nz>
2025-05-23 01:16:00 +1200

    Created a Hard level (yet to implement) and implemented the logic for the least used colour strategy

---
+++ src/main/java/nz/ac/auckland/se281/engine/Hard.java
@@ -0,0 +1,10 @@
+package nz.ac.auckland.se281.engine;
+
+public class Hard implements Level {
+
+  @Override
+  public Strategy decideStrategy() {
+    // TODO Auto-generated method stub
+    throw new UnsupportedOperationException("Unimplemented method 'decideStrategy'");
+  }
+}
---
+++ src/main/java/nz/ac/auckland/se281/engine/LeastUsedColourStrategy.java
@@ -0,0 +1,17 @@
+package nz.ac.auckland.se281.engine;
+
+import nz.ac.auckland.se281.model.Colour;
+
+public class LeastUsedColourStrategy implements Strategy {
+  private GameStats stats;
+
+  public LeastUsedColourStrategy(GameStats stats) {
+    this.stats = stats;
+  }
+
+  @Override
+  public Colour[] decideColours() {
+    // Return a random colour and guess the least used colour
+    return new Colour[] {Colour.getRandomColourForAi(), stats.getLeastUsedColour()};
+  }
+}
