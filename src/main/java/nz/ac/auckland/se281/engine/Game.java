c20581b3f037e9ac18b1dad99faf713d6337529f
nicholas-garcia-scholtz <ngar756@aucklanduni.ac.nz>
2025-05-22 16:06:05 +1200

    Added welcome message to program

--- src/main/java/nz/ac/auckland/se281/engine/Game.java
+++ src/main/java/nz/ac/auckland/se281/engine/Game.java
@@ -1,13 +1,17 @@
 package nz.ac.auckland.se281.engine;
 
 import nz.ac.auckland.se281.Main.Difficulty;
+import nz.ac.auckland.se281.cli.MessageCli;
 
 public class Game {
   public static String AI_NAME = "HAL-9000";
 
   public Game() {}
 
-  public void newGame(Difficulty difficulty, int numRounds, String[] options) {}
+  public void newGame(Difficulty difficulty, int numRounds, String[] options) {
+    String namePlayer = options[0];
+    MessageCli.WELCOME_PLAYER.printMessage(namePlayer);
+  }
 
   public void play() {}
 
